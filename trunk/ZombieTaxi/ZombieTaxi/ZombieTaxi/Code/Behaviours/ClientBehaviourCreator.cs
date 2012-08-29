a74875824b3fa5cf4d0594f65e44f2317717e81b
matt.hughson <matt.hughson@fd8718a5-8c53-0e06-f2cb-9c207c698f4d>
2012-08-29 05:57:09 +0000

    StonePickup can now be ... picked up! This is done through a new behaviour "Pickup" which checks for a collision with the player, and when it occurs, adds itself to the player's inventory.

--- trunk/ZombieTaxi/ZombieTaxi/ZombieTaxi/Code/Behaviours/ClientBehaviourCreator.cs
+++ trunk/ZombieTaxi/ZombieTaxi/ZombieTaxi/Code/Behaviours/ClientBehaviourCreator.cs
@@ -76,6 +76,10 @@ namespace ZombieTaxi.Behaviours
                     {
                         return new Inventory(go, fileName);
                     }
+                case "ZombieTaxi.Behaviours.Pickup":
+                    {
+                        return new Pickup(go, fileName);
+                    }
                 default:
                     {
                         return null;
