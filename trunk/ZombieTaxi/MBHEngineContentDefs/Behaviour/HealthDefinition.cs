56a7b008101c4d1ceae2224539017d1de3570d43
matt.hughson <matt.hughson@fd8718a5-8c53-0e06-f2cb-9c207c698f4d>
2012-10-26 06:23:39 +0000

    SpawnOnDeath has the Template it was spawning hard coded. Updated to be driven through XML.
    Health now has a flag to kill mParenGOH when reaching zero health. Didn't end up using it, but left in for future use.
    
    SpawnOnDeath now allows new object to be spawned at attachment point. Used for Wall since it is centered, and StonePickup is not.

--- trunk/ZombieTaxi/MBHEngineContentDefs/Behaviour/HealthDefinition.cs
+++ trunk/ZombieTaxi/MBHEngineContentDefs/Behaviour/HealthDefinition.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Content;
 
 namespace MBHEngineContentDefs
 {
@@ -16,5 +17,11 @@ namespace MBHEngineContentDefs
         /// The maxium amount of health this game object can store.
         /// </summary>
         public Single mMaxHealth;
+
+        /// <summary>
+        /// True if this object should be deleted when it reaches Zero Health.
+        /// </summary>
+        [ContentSerializer(Optional = true)]
+        public Boolean mRemoveOnDeath;
     }
 }
