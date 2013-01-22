49a9be43cad9534b7822e40f0a8a047e57e158a4
matt.hughson <matt.hughson@fd8718a5-8c53-0e06-f2cb-9c207c698f4d>
2013-01-22 07:48:57 +0000

    Civilian find place to stand in safehouse rather than just wandering.
    This exposed a bit of a bug in the Follow logic where in certain cases the civilians would get stuck on walls. This happened when the civilian position was slightly off, and so the path finder though he was standing on a wall and so the path search fails out right away. To fix, I use to collision center point instead, which is much closer to the center of the tile.

--- trunk/ZombieTaxi/ZombieTaxi/ZombieTaxi/Code/Behaviours/Civilian.cs
+++ trunk/ZombieTaxi/ZombieTaxi/ZombieTaxi/Code/Behaviours/Civilian.cs
@@ -120,7 +120,7 @@ namespace ZombieTaxi.Behaviours
                 mSetActiveAnimationMsg.mAnimationSetName = "Run";
                 pParentGOH.OnMessage(mSetActiveAnimationMsg);
 
-                mSetSourceMsg.mSource = pParentGOH.pPosition;
+                mSetSourceMsg.mSource = pParentGOH.pCollisionRect.pCenterPoint;
                 pParentGOH.OnMessage(mSetSourceMsg);
                 mSetDestinationMsg.mDestination = GameObjectManager.pInstance.pPlayer.pPosition;
                 pParentGOH.OnMessage(mSetDestinationMsg);
@@ -148,7 +148,7 @@ namespace ZombieTaxi.Behaviours
                     GameObjectManager.pInstance.BroadcastMessage(mIncrementScoreMsg, pParentGOH);
 
                     // Now just stand around for a little bit.
-                    return "WaitInSafeHouse";
+                    return "GoToStandingPosition";
                 }
                 // Has the Player run too far away causing us to get scared?
                 else if (Vector2.DistanceSquared(GameObjectManager.pInstance.pPlayer.pPosition, pParentGOH.pPosition) > 64 * 64)
@@ -529,6 +529,178 @@ namespace ZombieTaxi.Behaviours
         /// State where the Game Object stands in place waiting for the target to get far enough away
         /// to trigger a transition back to the follow state.
         /// </summary>
+        private class FSMStateGoToStandingPosition : FSMState
+        {
+            /// <summary>
+            /// Preallocate messages to avoid GC.
+            /// </summary>
+            private SpriteRender.SetActiveAnimationMessage mSetActiveAnimationMsg;
+            private PathFind.SetDestinationMessage mSetDestinationMsg;
+            private PathFind.SetSourceMessage mSetSourceMsg;
+            private PathFind.GetCurrentBestNodeMessage mGetCurrentBestNodeMsg;
+            private PathFind.ClearDestinationMessage mClearDestinationMsg;
+
+            /// <summary>
+            /// Constructor.
+            /// </summary>
+            public FSMStateGoToStandingPosition()
+            {
+                mSetActiveAnimationMsg = new SpriteRender.SetActiveAnimationMessage();
+                mSetDestinationMsg = new PathFind.SetDestinationMessage();
+                mSetSourceMsg = new PathFind.SetSourceMessage();
+                mGetCurrentBestNodeMsg = new PathFind.GetCurrentBestNodeMessage();
+                mClearDestinationMsg = new PathFind.ClearDestinationMessage();
+            }
+
+            /// <summary>
+            /// Called once when the state starts.
+            /// </summary>
+            public override void OnBegin()
+            {
+                mSetActiveAnimationMsg.mAnimationSetName = "Run";
+                pParentGOH.OnMessage(mSetActiveAnimationMsg);
+
+                // Use the collision center point since the position root is at the
+                // bottom of the image and can result in off by 1 errors when finding
+                // which tile this guy is standing in.
+                mSetSourceMsg.mSource = pParentGOH.pCollisionRect.pCenterPoint;
+                pParentGOH.OnMessage(mSetSourceMsg);
+
+                List<GameObject> safeHouses = GameObjectManager.pInstance.GetGameObjectsOfClassification(GameObjectDefinition.Classifications.SAFE_HOUSE);
+
+                Int32 index = RandomManager.pInstance.RandomNumber() % safeHouses.Count;
+
+                mSetDestinationMsg.mDestination = safeHouses[index].pPosition;
+                pParentGOH.OnMessage(mSetDestinationMsg);
+            }
+
+            /// <summary>
+            /// Call repeatedly until it returns a valid new state to transition to.
+            /// </summary>
+            /// <returns>Identifier of a state to transition to.</returns>
+            public override string OnUpdate()
+            {
+                if(Follow())
+                {
+                    return "WaitAtStandingPosition";
+                }
+
+                return null;
+            }
+
+            /// <summary>
+            /// Called once when leaving this state.  Called the frame after the Update which returned
+            /// a valid state to transition to.
+            /// </summary>
+            public override void OnEnd()
+            {
+                pParentGOH.OnMessage(mClearDestinationMsg);
+
+                // Clear the forward direction of this object so that it doesn't keep moving.
+                pParentGOH.pDirection.mForward = Vector2.Zero;
+            }
+
+            /// <summary>
+            /// Logic for basic follow behaviour.  Uses path find behaviour to follow the player.
+            /// </summary>
+            /// <remarks>
+            /// This is almost identicle to what is found in the Kamikaze Behaviour.  They should be combined.
+            /// </remarks>
+            private Boolean Follow()
+            {
+                //GameObject player = GameObjectManager.pInstance.pPlayer;
+
+                // Get the curent path to the player. It may not be complete at this point, but should include enough
+                // information to start moving.
+                pParentGOH.OnMessage(mGetCurrentBestNodeMsg);
+
+                // If we have a best node chosen (again maybe not a complete path, but the best so far), start
+                // moving towards the next point on the path.
+                if (mGetCurrentBestNodeMsg.mBest != null)
+                {
+                    // This is the node closest to the destination that we have found.
+                    PathFind.PathNode p = mGetCurrentBestNodeMsg.mBest;
+
+                    // Traverse back towards the source node until the previous one has already been reached.
+                    // That means the current one is the next one that has not been reached yet.
+                    // We also want to make sure we don't try to get to the starting node since we should be 
+                    // standing on top of it already (hence the check for prev.prev).
+                    while (p.mPrev != null && p.mPrev.mPrev != null && !p.mPrev.mReached)
+                    {
+                        p = p.mPrev;
+                    }
+
+                    if (p.mReached)
+                    {
+                        return true;
+                    }
+
+                    // The distance to check agaist is based on the move speed, since that is the amount
+                    // we will move this frame, and we want to avoid trying to hit the center point directly, since
+                    // that will only happen if moving in 1 pixel increments.
+                    // Also, we check double move speed because we are going to move this frame no matter what,
+                    // so what we are really checking is, are we going to be ther NEXT update.
+                    Single minDist = pParentGOH.pDirection.mSpeed * 2.0f;
+
+                    // Once we are within one unit of the target consider it reached.
+                    if (Vector2.Distance(p.mTile.mCollisionRect.pCenterBottom, pParentGOH.pPosition) <= minDist)
+                    {
+                        // This node has been reached, so next update it will start moving towards the next node.
+                        p.mReached = true;
+
+                        // Recalculate the path every time we reach a node in the path.  This accounts for things like
+                        // the target moving.
+                        //DebugMessageDisplay.pInstance.AddConstantMessage("Reached target.  Setting new destination.");
+                    }
+
+                    //DebugMessageDisplay.pInstance.AddConstantMessage("Moving towards target.");
+
+                    // Move towards the nodes center point.
+                    Vector2 d = p.mTile.mCollisionRect.pCenterBottom - pParentGOH.pPosition;
+                    if (d.Length() != 0.0f)
+                    {
+                        d = Vector2.Normalize(d);
+                        pParentGOH.pDirection.mForward = d;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// State where the Game Object stands in place waiting for the target to get far enough away
+        /// to trigger a transition back to the follow state.
+        /// </summary>
+        private class FSMStateWaitAtStandingPosition : FSMState
+        {
+            /// <summary>
+            /// Preallocate messages to avoid GC.
+            /// </summary>
+            private SpriteRender.SetActiveAnimationMessage mSetActiveAnimationMsg;
+
+            /// <summary>
+            /// Constructor.
+            /// </summary>
+            public FSMStateWaitAtStandingPosition()
+            {
+                mSetActiveAnimationMsg = new SpriteRender.SetActiveAnimationMessage();
+            }
+
+            /// <summary>
+            /// Called once when the state starts.
+            /// </summary>
+            public override void OnBegin()
+            {
+                mSetActiveAnimationMsg.mAnimationSetName = "Idle";
+                pParentGOH.OnMessage(mSetActiveAnimationMsg);
+            }
+        }
+
+        /// <summary>
+        /// State where the Game Object stands in place waiting for the target to get far enough away
+        /// to trigger a transition back to the follow state.
+        /// </summary>
         private class FSMStateDead : FSMState
         {
             /// <summary>
@@ -739,6 +911,8 @@ namespace ZombieTaxi.Behaviours
             AddState(new FSMStateStay(), "Stay");
             AddState(new FSMStateWaitInSafeHouse(), "WaitInSafeHouse");
             AddState(new FSMStateWanderInSafeHouse(), "WanderInSafeHouse");
+            AddState(new FSMStateGoToStandingPosition(), "GoToStandingPosition");
+            AddState(new FSMStateWaitAtStandingPosition(), "WaitAtStandingPosition");
             AddState(new FSMStateDead(), "Dead");
             AddState(new FSMStateGoToExtraction(), "GoToExtraction");
 
@@ -788,9 +962,13 @@ namespace ZombieTaxi.Behaviours
                 ExtractionPoint.OnExtractionPointActivatedMessage temp = (ExtractionPoint.OnExtractionPointActivatedMessage)msg;
                 mExtractionPoint = msg.pSender;
 
+                FSMState curState = GetCurrentState();
+
                 // If anyone is in the Safe House, they should make a run for the Extraction point now.
-                if (GetCurrentState() is FSMStateWaitInSafeHouse ||
-                    GetCurrentState() is FSMStateWanderInSafeHouse)
+                if (curState is FSMStateWaitInSafeHouse ||
+                    curState is FSMStateWanderInSafeHouse ||
+                    curState is FSMStateGoToStandingPosition ||
+                    curState is FSMStateWaitAtStandingPosition)
                 {
                     AdvanceToState("GoToExtraction");
                 }
