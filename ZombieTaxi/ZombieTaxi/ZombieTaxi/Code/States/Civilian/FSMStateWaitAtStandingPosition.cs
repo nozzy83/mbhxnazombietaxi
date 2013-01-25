e2ec981b2783a2a1d5246b0d65d9155728a55f52
matt.hughson <matt.hughson@fd8718a5-8c53-0e06-f2cb-9c207c698f4d>
2013-01-25 07:20:08 +0000

    Multiple issues with Civilian in SafeHouse:
    1) Occupied tiles now have that flag removed when the Civilian leaves the tile.
    2) GetGameObjectsOfClassification was returning a reference to the list, and we were subsequently removing items from that list (thinking it was only the local copy). Over time all SafeHouses were removed from that list. We now copy the valid entries into a new list instead of removing them from the source list.

--- ZombieTaxi/ZombieTaxi/ZombieTaxi/Code/States/Civilian/FSMStateWaitAtStandingPosition.cs
+++ ZombieTaxi/ZombieTaxi/ZombieTaxi/Code/States/Civilian/FSMStateWaitAtStandingPosition.cs
@@ -1,5 +1,6 @@
 ﻿using MBHEngine.StateMachine;
 using MBHEngine.Behaviour;
+using MBHEngine.World;
 
 namespace ZombieTaxi.States.Civilian
 {
@@ -13,6 +14,7 @@ namespace ZombieTaxi.States.Civilian
         /// Preallocate messages to avoid GC.
         /// </summary>
         private SpriteRender.SetActiveAnimationMessage mSetActiveAnimationMsg;
+        private Level.GetTileAtObjectMessage mGetTileAtObjectMsg;
 
         /// <summary>
         /// Constructor.
@@ -20,6 +22,7 @@ namespace ZombieTaxi.States.Civilian
         public FSMStateWaitAtStandingPosition()
         {
             mSetActiveAnimationMsg = new SpriteRender.SetActiveAnimationMessage();
+            mGetTileAtObjectMsg = new Level.GetTileAtObjectMessage();
         }
 
         /// <summary>
@@ -29,6 +32,31 @@ namespace ZombieTaxi.States.Civilian
         {
             mSetActiveAnimationMsg.mAnimationSetName_In = "Idle";
             pParentGOH.OnMessage(mSetActiveAnimationMsg);
+
+            // Grab the tile at that location and update its Attributes to now be
+            // Occupied.
+            mGetTileAtObjectMsg.Reset();
+            mGetTileAtObjectMsg.mObject_In = pParentGOH;
+            WorldManager.pInstance.pCurrentLevel.OnMessage(mGetTileAtObjectMsg);
+
+            if (null != mGetTileAtObjectMsg.mTile_Out)
+            {
+                mGetTileAtObjectMsg.mTile_Out.SetAttribute(Level.Tile.Attribute.Occupied);
+            }
+        }
+
+        /// <summary>
+        /// Called once when leaving this state.  Called the frame after the Update which returned
+        /// a valid state to transition to.
+        /// </summary>
+        public override void OnEnd()
+        {
+            if (null != mGetTileAtObjectMsg.mTile_Out)
+            {
+                // This would have been set in OnBegin. It will be set again if we are transitioning
+                // to 
+                mGetTileAtObjectMsg.mTile_Out.ClearAttribute(Level.Tile.Attribute.Occupied);
+            }
         }
     }
 }
