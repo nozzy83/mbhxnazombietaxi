d614d322cb1e37ecb40e9f73ce21ad8ef0eee52b
matt.hughson <matt.hughson@fd8718a5-8c53-0e06-f2cb-9c207c698f4d>
2013-03-29 18:07:12 +0000

    Task: Limit object placement range.
    Object Placement is now limited by a specified number of tiles.
    
    https://app.asana.com/0/1194642625728/4745981848699

--- ZombieTaxi/ZombieTaxiContentDefs/Behaviour/ObjectPlacementDefinition.cs
+++ ZombieTaxi/ZombieTaxiContentDefs/Behaviour/ObjectPlacementDefinition.cs
@@ -8,5 +8,10 @@ namespace ZombieTaxiContentDefs
 {
     public class ObjectPlacementDefinition : BehaviourDefinition
     {
+        /// <summary>
+        /// The limits of how far the cursor can be offset from the player. These are absolute values, and should
+        /// never be negative.
+        /// </summary>
+        public Microsoft.Xna.Framework.Point mAbsOffsetRange;
     }
 }
