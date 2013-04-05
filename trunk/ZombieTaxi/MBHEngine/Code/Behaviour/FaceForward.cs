11d8ca0c958255febb5270d8bab185551acdab24
matt.hughson <matt.hughson@fd8718a5-8c53-0e06-f2cb-9c207c698f4d>
2013-04-05 06:01:31 +0000

    Task: Militant Stranded type (full imp minus weapon upgrades).
    Added Behaviour for pointing and shooting to the Militant.
    
    Created new guns and bullets for the militant which acts more like a high powered rifle.

--- trunk/ZombieTaxi/MBHEngine/Code/Behaviour/FaceForward.cs
+++ trunk/ZombieTaxi/MBHEngine/Code/Behaviour/FaceForward.cs
@@ -9,10 +9,36 @@ using Microsoft.Xna.Framework.Graphics;
 namespace MBHEngine.Behaviour
 {
     /// <summary>
-    /// Forces a sprite to face the direction it is currently moving.
+    /// Simple behvaiour to force a sprite to face the direction it is moving.
+    /// Also can optionally be forced to face a specific target when needed.
     /// </summary>
     public class FaceForward : MBHEngine.Behaviour.Behaviour
     {
+        /// <summary>
+        /// Forces a sprite to face the direction it is currently moving.
+        /// </summary>
+        public class SetLookTargetMessage : BehaviourMessage
+        {
+            /// <summary>
+            /// The object to look at regardless of move direction.
+            /// Set to null to return to standard behaviour.
+            /// </summary>
+            public MBHEngine.GameObject.GameObject mTarget_In;
+
+            /// <summary>
+            /// Put message back into a default state.
+            /// </summary>
+            public override void Reset()
+            {
+                mTarget_In = null;
+            }
+        }
+
+        /// <summary>
+        /// An optional GameObject to face regardless of movement direction.
+        /// </summary>
+        private MBHEngine.GameObject.GameObject mTarget;
+
         /// <summary>
         /// Preallocate messages to avoid GC.
         /// </summary>
@@ -51,15 +77,43 @@ namespace MBHEngine.Behaviour
         {
             base.PostUpdate(gameTime);
 
-            if (mParentGOH.pDirection.mForward.X < 0)
+            Single dir = mParentGOH.pDirection.mForward.X;
+
+            if (null != mTarget)
+            {
+                dir = mTarget.pPosition.X - mParentGOH.pPosition.X;
+            }
+
+            if (dir < 0)
             {
                 mSetSpriteFxMsg.mSpriteEffects_In = SpriteEffects.FlipHorizontally;
                 mParentGOH.OnMessage(mSetSpriteFxMsg);
             }
-            else if (mParentGOH.pDirection.mForward.X > 0)
+            else if (dir > 0)
             {
                 mSetSpriteFxMsg.mSpriteEffects_In = SpriteEffects.None;
                 mParentGOH.OnMessage(mSetSpriteFxMsg);
+            }
+        }
+
+        /// <summary>
+        /// The main interface for communicating between behaviours.  Using polymorphism, we
+        /// define a bunch of different messages deriving from BehaviourMessage.  Each behaviour
+        /// can then check for particular upcasted messahe types, and either grab some data 
+        /// from it (set message) or store some data in it (get message).
+        /// </summary>
+        /// <param name="msg">The message being communicated to the behaviour.</param>
+        public override void OnMessage(ref BehaviourMessage msg)
+        {
+            if (msg is SetLookTargetMessage)
+            {
+                SetLookTargetMessage temp = (SetLookTargetMessage)msg;
+
+                mTarget = temp.mTarget_In;
+            }
+            else if (msg is Health.OnZeroHealthMessage)
+            {
+                mParentGOH.SetBehaviourEnabled<FaceForward>(false);
             }
         }
     }
