26aa2e5887fc67219c6f69a41b2a4c361f807c5f
matt.hughson <matt.hughson@fd8718a5-8c53-0e06-f2cb-9c207c698f4d>
2013-04-17 03:41:50 +0000

    Task: Militant Stranded type (full imp minus weapon upgrades).
    Brought a bunch of functionality over from Civilian to the Militant:
    
      - Moving to extraction point when one is placed.
      - Getting points for bringing to SafeHouse.
      - Scouts can find Militant.
    
    https://app.asana.com/0/1194642625728/1260755855320

--- ZombieTaxi/ZombieTaxi/ZombieTaxi/Code/Behaviours/FSMMilitant.cs
+++ ZombieTaxi/ZombieTaxi/ZombieTaxi/Code/Behaviours/FSMMilitant.cs
@@ -3,6 +3,8 @@ using MBHEngine.Behaviour;
 using Microsoft.Xna.Framework;
 using MBHEngine.GameObject;
 using ZombieTaxi.States.Militant;
+using MBHEngine.StateMachine;
+using ZombieTaxiContentDefs;
 
 namespace ZombieTaxi.Behaviours
 {
@@ -11,6 +13,17 @@ namespace ZombieTaxi.Behaviours
     /// </summary>
     class FSMMilitant : MBHEngine.Behaviour.FiniteStateMachine
     {
+        /// <summary>
+        /// The currently active extraction point.
+        /// </summary>
+        private GameObject mExtractionPoint;
+
+        /// <summary>
+        /// The number of points awarded when this GameObject arrives
+        /// at a SafeHouse.
+        /// </summary>
+        private Int32 mSafeHouseScore;
+
         /// <summary>
         /// Constructor which also handles the process of loading in the Behaviour
         /// Definition information.
@@ -30,6 +43,8 @@ namespace ZombieTaxi.Behaviours
         {
             base.LoadContent(fileName);
 
+            FSMMilitantDefinition def = GameObjectManager.pInstance.pContentManager.Load<FSMMilitantDefinition>(fileName);
+
             AddState(new FSMStateCower(), "Cower");
             AddState(new FSMStateFollowTarget(), "Follow");
             AddState(new FSMStateStay(), "Stay");
@@ -42,6 +57,8 @@ namespace ZombieTaxi.Behaviours
             AddState(new FSMStatePauseAtPatrolPoint(), "PatrolPause");
 
             mParentGOH.pDirection.mSpeed = 0.625f;
+
+            mSafeHouseScore = def.mSafeHouseScore;
         }
 
         /// <summary>
@@ -67,6 +84,45 @@ namespace ZombieTaxi.Behaviours
             if (msg is Health.OnZeroHealthMessage)
             {
                 AdvanceToState("Dead");
+            }
+            else if (msg is ExtractionPoint.OnExtractionPointActivatedMessage)
+            {
+                // Store the next extraction point.
+                ExtractionPoint.OnExtractionPointActivatedMessage temp = (ExtractionPoint.OnExtractionPointActivatedMessage)msg;
+                mExtractionPoint = msg.pSender;
+
+                FSMState curState = GetCurrentState();
+
+                // If anyone is in the Safe House, they should make a run for the Extraction point now.
+                if (curState is States.Civilian.FSMStateGoToStandingPosition ||
+                    curState is FSMStateWaitAtStandingPosition ||
+                    curState is FSMStatePatrol ||
+                    curState is FSMStatePauseAtPatrolPoint)
+                {
+                    AdvanceToState("GoToExtraction");
+                }
+            }
+            else if (msg is Civilian.GetExtractionPointMessage)
+            {
+                Civilian.GetExtractionPointMessage temp = (Civilian.GetExtractionPointMessage)msg;
+                temp.mExtractionPoint_Out = mExtractionPoint;
+            }
+            else if (msg is Civilian.GetSafeHouseScoreMessage)
+            {
+                Civilian.GetSafeHouseScoreMessage temp = (Civilian.GetSafeHouseScoreMessage)msg;
+                temp.mSafeHouseScore_Out = mSafeHouseScore;
+            }
+            else if (msg is StrandedPopup.GetIsScoutableMessage)
+            {
+                // Currently the only thing required for a Stranded to be Scoutable, is that
+                // they are currently in the Cower state. This can later be expanded to include
+                // things like distance from the sender.
+                if (GetCurrentState() is FSMStateCower)
+                {
+                    StrandedPopup.GetIsScoutableMessage temp = (StrandedPopup.GetIsScoutableMessage)msg;
+
+                    temp.mIsScoutable_Out = true;
+                }
             }
         }
     }
